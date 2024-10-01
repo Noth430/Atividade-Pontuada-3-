6e95c47f4e4696517cf7094fd9f0ecfbf479c778
Noth430 <henrique2.095@outlook.com>
2024-10-01 16:47:51 +0000

    salvar

--- Cod Principal.py
+++ Cod Principal.py
@@ -1,3 +1,23 @@
 import os
 os.system("cls || clear")
 
+""""
+    A prefeitura de uma cidade fez uma pesquisa entre seus habitantes, coletando dados sobre o salário e número de filhos das famílias da cidade. Aprefeitura deseja saber
+
+a) total de famílias que responderam a pesquisa;
+b)média do salário da população;
+c) média do números de filhos;
+d) maior salário;
+e) menor salário.
+
+Crie um menu com as seguintes opções.
+código | descrição
+  1       Adicionar família
+  2       Exibir rsultados
+  3       Sair
+
+Ao adicionar uma família, deve-se limpar o terminal e apresentar o menu novamente.
+1. Salve os dados em um arquivo com nome: pesquisa_prefeitura.txt
+2. O programa deve ler o arquivo para exibir os dados salvos.
+"""
+
